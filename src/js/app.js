b9f68196c8adbf3413bf68cf57ec33203ab59cb5
MariaVictoriaZ <163335173+MariaVictoriaZ@users.noreply.github.com>
2024-05-10 14:28:30 +0000

    First Commit

--- src/js/app.js
+++ src/js/app.js
@@ -29,13 +29,19 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  let name = variables.name !== null ? variables.name : "Name";
+  let lastName = variables.lastName !== null ? variables.lastName : "Last Name";
+  let role = variables.role !== null ? variables.role : "Your role";
+  let city = variables.city !== null ? variables.city : "Your city,";
+  let country = variables.country !== null ? variables.country : "Your country";
+
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h1>${name} ${lastName}</h1>
+          <h2>${role}</h2>
+          <h3>${city} ${country}</h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
